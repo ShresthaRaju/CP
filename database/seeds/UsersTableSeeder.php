55963179f85cc1054c01f7302a9264b0a96b1f97
ShresthaRaju <rajshres07@gmail.com>
2018-04-29 11:27:59 +0545

    Configurations for heroku

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,46 +11,137 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // factory(App\User::class, 10)->create();
-
         DB::table('users')->insert([
 
-          'name'=>'Raj',
-          'email'=>'raj@example.com',
-          'username'=>'raj07',
-          'password'=>bcrypt('secret'),
-          'display_image'=>'dp.jpg',
-          'github'=>'ShresthaRaju',
-          'linkedin'=>'rajushrestha7',
-          'active'=>1,
-          'created_at'=>\Carbon\Carbon::now(),
-          'updated_at'=>\Carbon\Carbon::now()
+          [
+            'name'=>'Raj',
+            'email'=>'raj@example.com',
+            'username'=>'raj',
+            'password'=>bcrypt('secret'),
+            'display_image'=>'dp.jpg',
+            'github'=>'ShresthaRaju',
+            'linkedin'=>'rajushrestha7',
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
 
-          // [
-          //   'name'=>'Raj Shrestha',
-          //   'email'=>'raj@example.com',
-          //   'username'=>'raj',
-          //   'password'=>bcrypt('secret'),
-          //   'display_image'=>'dp.jpg',
-          //   'github'=>'ShresthaRaju',
-          //   'linkedin'=>'rajushrestha7',
-          //   'active'=>1,
-          //   'created_at'=>\Carbon\Carbon::now(),
-          //   'updated_at'=>\Carbon\Carbon::now()
-          // ],
+          [
+            'name'=>'Sameer',
+            'email'=>'sameer@example.com',
+            'username'=>'sameer',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
 
-          // [
-          //   'name'=>'Sameer',
-          //   'email'=>'sameer@example.com',
-          //   'username'=>'sameer',
-          //   'password'=>bcrypt('secret'),
-          //   'display_image'=>null,
-          //   'github'=>null,
-          //   'linkedin'=>null,
-          //   'active'=>1,
-          //   'created_at'=>\Carbon\Carbon::now(),
-          //   'updated_at'=>\Carbon\Carbon::now()
-          // ]
+          [
+            'name'=>'Khem Gurung',
+            'email'=>'khem@example.com',
+            'username'=>'khem',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Bhupendra',
+            'email'=>'vupen@example.com',
+            'username'=>'vupen',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Karuna',
+            'email'=>'karuna@example.com',
+            'username'=>'karuna',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Alina',
+            'email'=>'alina@example.com',
+            'username'=>'alina',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Suman',
+            'email'=>'suman@example.com',
+            'username'=>'suman',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Yogendra',
+            'email'=>'yogen@example.com',
+            'username'=>'yogen',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Meelan',
+            'email'=>'meelan@example.com',
+            'username'=>'meelan',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ],
+
+          [
+            'name'=>'Hem',
+            'email'=>'hem@example.com',
+            'username'=>'hem',
+            'password'=>bcrypt('secret'),
+            'display_image'=>null,
+            'github'=>null,
+            'linkedin'=>null,
+            'active'=>1,
+            'created_at'=>\Carbon\Carbon::now(),
+            'updated_at'=>\Carbon\Carbon::now()
+          ]
 
         ]);
     }
